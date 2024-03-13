7a0712705575225841aba3b0885b7f1061d400a2
stefanovagloria <gloriqstefanova97@gmail.com>
2024-03-13 07:31:04 +0300

    form transformed to controlled component

--- src/components/AdminPanel/AddProcedure/AddProcedure.jsx
+++ src/components/AdminPanel/AddProcedure/AddProcedure.jsx
@@ -4,45 +4,112 @@ import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import useMediaQuery from "@mui/material/useMediaQuery";
+import { useState } from "react";
 
 const AddProcedure = ({ show, hide, category }) => {
+  const [procedureValues, setProcedureValues] = useState({
+    name: "",
+    photos: [],
+    price: '',
+    promoPrice: '',
+    characteristics: [{ key: "", value: null }],
+    description: "",
+    relatedProducts: [],
+  });
+
+  const onRemove = (index) => {};
+
+  const onCharacteristicsChange = (e) => {};
+
+  const onChangeHandler = (e) => {
+    const inputName = e.target.name;
+    console.log(e.target.value)
+    if (inputName !== "photos") {
+      console.log('setValues..')
+      setProcedureValues((values) => ({
+        ...values,
+        [inputName]: e.target.value,
+      }));
+    }
+  };
+
   return (
     <Dialog
       fullScreen={fullScreen}
       open={show}
       onClose={hide}
       aria-labelledby="responsive-dialog-title"
-      
     >
       <DialogContent>
         <form className={styles.container}>
           <div className={styles.fields}>
-            <label>Име на процедура:</label>
-            <input />
+            <label htmlFor="name">Име на процедура:</label>
+            <input
+              id="name"
+              name="name"
+              value={procedureValues.name}
+              onChange={onChangeHandler}
+            />
           </div>
           <div className={styles.category}>{category}</div>
           <div className={styles.fields}>
-            <label> Снимки на процедурата</label>
-            <input type="file" />
+            <label htmlFor="photos"> Снимки на процедурата</label>
+            <input
+              id="photos"
+              name="photos"
+              type="file"
+              onChange={onChangeHandler}
+            />
           </div>
-          <div >
-            <label>Цена:</label>
-            <input />
+          <div>
+            <label htmlFor="price">Цена:</label>
+            <input
+              id="price"
+              name="price"
+              value={procedureValues.price}
+              onChange={onChangeHandler}
+            />
           </div>
-          <div >
-          <label>Промоционална цена:</label>
-          <input />
+          <div>
+            <label htmlFor="promoPrice">Промоционална цена:</label>
+            <input
+              id="promoPrice"
+              name="promoPrice"
+              value={procedureValues.promoPrice}
+              onChange={onChangeHandler}
+            />
           </div>
           <div className={styles.fields}>
-          <label >Характеристики:</label>
-          <input />
+            <label>Характеристики:</label>
+            <div>
+              {procedureValues.length > 0 &&
+                procedureValues.characteristics.map((ch, index) => (
+                  <div key={index}>
+                    <input
+                      value={ch.key}
+                      onChange={(e) =>
+                        onCharacteristicsChange(index, e.target.value, ch.value)
+                      }
+                    />
+                    <input
+                      value={ch.value}
+                      onChange={(e) =>
+                        onCharacteristicsChange(index, ch.key, e.target.value)
+                      }
+                    />
+                    <Button onClick={() => onRemove(index)}>Remove</Button>
+                  </div>
+                ))}
+            </div>
           </div>
           <div className={styles.fields}>
-          <label>Описание на продукт:</label>
-          <textarea />
+            <label htmlFor="description">Описание на продукт:</label>
+            <textarea
+              id="description"
+              name="description"
+              value={procedureValues.description}
+              onChange={onChangeHandler}
+            />
           </div>
           <div className={styles.fields}>
             Сходни продукти:
